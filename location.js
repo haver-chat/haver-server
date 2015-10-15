bf6c96d0fe87e57781bdfd5a262a128b96c02458
azertify <az@twist.moe>
2015-10-15 23:37:39 +0100

    Fixed map API

--- location.js
+++ location.js
@@ -1,4 +1,4 @@
-var Location = function() {
+var Position = function() {
   var _this = this;
   this.longitude = 0;
   this.latitude = 0;
@@ -27,10 +27,40 @@ var Location = function() {
 }
 
 var map;
-var location = new Location();
+var pos = new Position();
 function initMap() {
+  var coords = {lat: pos.latitude, lng: pos.longitude}
   map = new google.maps.Map(document.querySelector("#map"), {
-    center: {lat: location.latitude, lng: location.longitude},
-    zoom: 8
+    center: coords,
+    zoom: 16  
   });
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    title: 'Hello World!'
+  });
+  var accuracyRadius = new google.maps.Circle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    map: map,
+    center: coords,
+    radius: pos.accuracy
+  });
+  document.querySelector("#map").style.height = window.innerHeight + "px";
+  console.log("Latitude: " + pos.latitude);
+  console.log("Longitude: " + pos.longitude);
 }
+
+
+function getCurrentPosition(callback) {
+  // find data
+  var data = { 'whatever': 0 };
+  callback(data);
+}
+
+var whatever = function(pos) {
+  console.log(JSON.stringify(pos));
+}
+
+getCurrentPosition(whatever);
