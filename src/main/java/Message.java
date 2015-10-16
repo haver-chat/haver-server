132a795f8f337d830caba8084993f8be2a72c3b5
Edward Knight <edw@rdknig.ht>
2015-10-16 18:36:16 +0100

    Made Message more useless, but it compiles =)

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,7 +1,9 @@
 import org.json.simple.JSONObject;
 
+/**
+ * All Messages have a constructor with the signature:
+ * 	public CLASS(JSONObject jsonObject)
+ */
 public abstract class Message {
 	public final static String KEY_TYPE = "type";
-
-	public Message(JSONObject jsonObject) {}
 }
