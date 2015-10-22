4cc0cbf86bf8b46ba072220d59d01c883db19b7d
azertify <az@twist.moe>
2015-10-22 16:34:57 +0100

    Add default config & file no longer req

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 /**
@@ -6,14 +7,22 @@ import java.io.FileReader;
  */
 public class Main {
     public static final boolean DEBUG = true;
+    private static final String CONFIG = "config.txt";
 
     public static void main(String[] args) {
         try {
-            BufferedReader r = new BufferedReader(new FileReader("config.txt"));
-            String file = "";
-            String line;
-            while((line = r.readLine()) != null) file += line;
-            Router router = new Router(file.split(":")[0], Integer.valueOf(file.split(":")[1]));
+            File f = new File(CONFIG);
+            String host = "127.0.0.1";
+            int port = 8080;
+            if (f.exists() && !f.isDirectory()) {
+                BufferedReader r = new BufferedReader(new FileReader(CONFIG));
+                String file = "";
+                String line;
+                while ((line = r.readLine()) != null) file += line;
+                host = file.split(":")[0];
+                port = Integer.valueOf(file.split(":")[1]);
+            }
+            Router router = new Router(host, port);
             router.start();
             System.out.println("Hosting new server on: " + router.getAddress());
         } catch (Exception e) {
