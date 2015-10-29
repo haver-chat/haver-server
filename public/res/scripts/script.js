f665b1d4d013141fdaae59c5d7016f20474e3894
azertify <az@twist.moe>
2015-10-29 17:30:27 +0000

    Refactor client

--- public/res/scripts/script.js
+++ public/res/scripts/script.js
@@ -1,179 +1,157 @@
-var Position = function(callback) {
-  var _this = this;
-  this.longitude = 0;
-  this.latitude = 0;
-  this.accuracy = -1;
-  this.callback = callback;
+var Chat = function() {
+  var exports = {};
   
-  this.setPosition = function(pos) {
-    _this.latitude = pos.coords.latitude;
-    _this.longitude = pos.coords.longitude;
-    _this.accuracy = pos.coords.accuracy;
-    if (typeof callback != 'undefined') {
-      callback(_this);
-    }
-  }
-  
-  this.reqPosition = function() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.setPosition);
-    } else {
-      // No geolocation, we dun goofed!
-    }
-  }
-  
-  this.hasPosition = function() {
-    return ~_this.accuracy;
-  }
-  
-  // setup:
-  this.reqPosition();
-}
-
-var Post = function(content, recipients) {
-  this.content = content;
-  this.to = typeof recipients != "undefined" ? recipients : [];
-}
-
-var RoomInfo = function(name, radius) {
-  this.name = name;
-  this.radius = radius;
-}
-
-var Socket = function() {
-  var _this = this;
-  this.socket = null;
-  this.types = {
+  var types = {
     LOCATION: 0,
     POST: 1,
     ROOM_INFO: 2,
     CLIENT_INFO: 3
   };
-  this.send = null;
   
-  this.connect = function() {
+  function getLocation(callback) {
+    function ret(pos) {
+      if (typeof callback != 'undefined') {
+        callback({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+          accuracy: pos.coords.accuracy
+        });
+      }
+    }
+    function req() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(ret);
+      } else {
+        console.log("No geolocation!"); // No geolocation, we dun goofed!
+      }
+    }
+    req();
+  }
+  
+  var Post = function(content, to) {
+    this.content = content.trim();
+    this.to = typeof to != "undefined" ? to : [];
+  }
+  
+  var RoomInfo = function(name, radius) {
+    this.name = name;
+    this.radius = radius;
+  }
+  
+  var socket = null;
+  
+  var open = function() {
+    console.log("Connected");
+    getLocation(function(pos) {
+      send(types.LOCATION, pos);
+    });
+  }
+  var close = function() {
+    console.log("Disconnected");
+  }
+  var error = function() {
+    console.log("Error");
+    close();
+  }
+  var message = function(m) {
+    var message = JSON.parse(m.data);
+    if (typeof message.type == 'undefined');
+    switch(message.type) {
+      case types.LOCATION:
+        getLocation(function(pos) {
+          send(types.LOCATION, pos);
+        });
+        break;
+      case types.POST:
+        receiveMessage(message.from, message.content);
+        break;
+      case types.ROOM_INFO:
+        var room = new RoomInfo("Test Room", 130);
+        send(types.ROOM_INFO, room);
+        break;
+      case types.CLIENT_INFO:
+        if (typeof message.clientName == 'undefined') {
+          for(var i = 0; i < message.names.length; i++) {
+            var status = message.change ? 'joined' : 'left';
+            receiveMessage('System', message.names[i] + ' has ' + status + ' the room');
+          }
+        } else {
+          receiveMessage('System', 'Welcome to ' + message.roomName + ', you are the ' + message.clientName);
+          message.names.splice(message.names.indexOf(message.clientName), 1);
+          if (message.names.length > 0) {
+            var names = 'Say hello to: ' + message.names[0];
+            for (var i = 1; i < message.names.length; i++) names += ', ' + message.names[i];
+            receiveMessage('System', names);
+          }
+        }
+      default:
+        break;
+    }
+  }
+  
+  var connect = function() {
     var protocol = location.protocol.split('http').join('ws') + '//';
     var host = (protocol == "file://") ? 'ws://127.0.0.1:8080' : protocol + location.host + '/soc';
-    _this.socket = new WebSocket(host);
-    
-    _this.socket.onopen = function() {
-      sendPos();
-    }
-    
-    _this.socket.onclose = function() {
-      addMessage({
-        from: 'System',
-        content: 'You have disconnected'
-      })
-      _this.reconnect();
-    }
-    
-    _this.socket.onmessage = function(res) {
-      var message = JSON.parse(res.data);
-      console.log(message);
-      var types = _this.types;
-      switch(message.type) {
-        case types.LOCATION:
-          sendPos();
-          break;
-        case types.POST:
-          addMessage(message);
-          break;
-        case types.ROOM_INFO:
-          var room = new RoomInfo("Essentials Queue", 150);
-          _this.send(_this.types.ROOM_INFO, room);
-          break;
-        case types.CLIENT_INFO:
-          if (typeof message.clientName != 'undefined') {
-            addMessage({
-              from: 'System',
-              content: 'Welcome to ' + message.roomName + ', you are the ' + message.clientName
-            });
-            message.names.splice(message.names.indexOf(message.clientName), 1);
-            if (message.names.length > 0) {
-              var names = 'Say hello to: ' + message.names[0];
-              for (var i = 1; i < message.names.length; i++) names += ', ' + message.names[i];
-              addMessage({
-                from: 'System',
-                content: names
-              });
-            }
-          } else {
-            for (var i = 0; i < message.names.length; i++) {
-              var status = message.change ? 'joined' : 'left';
-              addMessage({
-                from: 'System',
-                content: message.names[i] + ' has ' + status + ' the room'
-              });
-            }
-          }
-          break;
-        default:
-          break;
-      }
-    }
-    
-    _this.send = function(type, data) {
-      data['type'] = type;
-      console.log(JSON.stringify(data)); // log what we send
-      _this.socket.send(JSON.stringify(data));
-    }
-    
-    var sendPos = function() {
-      var send = function(pos) {
-        console.log("Sending position: " + pos);
-        _this.send(_this.types.LOCATION, pos);
-      }
-      var pos = new Position(send);
-    }
-    
-    var addMessage = function(message) {
-      var div = document.querySelector('#chat-list');
-      var bottom = div.scrollTop + div.offsetHeight === div.scrollHeight;
-      console.log("Adding messsage to UL");
-      var li = document.createElement('li');
-      li.innerHTML += "<span class=\"username\">" + escapeHtml(message.from) + "</span>: " + escapeHtml(message.content);
-      document.querySelector('#chat ul').appendChild(li);
-      if (bottom) div.scrollTop = div.scrollHeight - div.offsetHeight;
-    } 
-    
+    socket = new WebSocket(host);
+    socket.onopen = open;
+    socket.onclose = close;
+    socket.onerror = error;
+    socket.onmessage = message;
   }
   
-  this.reconnect = function() {
-    setTimeout(function() {
-      _this.connect();
-    }, 3000);
+  var send = function(type, message) {
+    message['type'] = type;
+    socket.send(JSON.stringify(message));
   }
-}
+  
+  function escapeHtml(unsafe) {
+    if (typeof unsafe == 'undefined') return '';
+    var safe = unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+    return safe;
+  }
+  
+  function receiveMessage(from, content) {
+    exports.onmessage(escapeHtml(from), escapeHtml(content));
+  }
+  
+  exports.onmessage = function(from, content) {
+    console.log(from + ": " + content);
+  }
+  exports.send = function(message) {
+    var post = new Post(message);
+    if (post.content.length == 0) return;
+    send(types.POST, post);
+  }
+  exports.connect = connect;
+  return exports;
+};
 
+// FRONT END:
+
+var chat = Chat();
+chat.connect();
+chat.onmessage = function(from, content) {
+  var div = document.querySelector('#chat-list');
+  var bottom = div.scrollTop + div.offsetHeight === div.scrollHeight;
+  console.log("Adding messsage to UL");
+  var li = document.createElement('li');
+  li.innerHTML += "<span class=\"username\">" + from + "</span>: " + content;
+  document.querySelector('#chat ul').appendChild(li);
+  if (bottom) div.scrollTop = div.scrollHeight - div.offsetHeight;
+}
 document.querySelector("#chat-form").onsubmit = function(e) {
   e.preventDefault();
   try {
-    var message = document.querySelector("input[name=msg-box]").value.trim();
-    if (message.length > 0) {
-      soc.send(soc.types.POST, new Post(message));
-      document.querySelector("input[name=msg-box]").value = "";
-    }
+    var message = document.querySelector("input[name=msg-box]").value;
+    chat.send(message);
+    document.querySelector("input[name=msg-box]").value = "";
   } catch(exception) {
     throw new Error(exception.message);
   }
   return false;
 }
-
-document.querySelector("#chat-form input").onfocus = function() {
-  var div = document.querySelector('#chat-list');
-  div.scrollTop = div.scrollHeight - div.offsetHeight; // scroll to bottom
-}
-
-var escapeHtml = function(unsafe) {
-  if (typeof unsafe == 'undefined') return;
-  return unsafe
-       .replace(/&/g, "&amp;")
-       .replace(/</g, "&lt;")
-       .replace(/>/g, "&gt;")
-       .replace(/"/g, "&quot;")
-       .replace(/'/g, "&#039;");
-}
-
-var soc = new Socket();
-soc.connect();
