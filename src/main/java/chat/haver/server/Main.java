f5a260f0a25aecac1c204e79a6cf778bd4bf2ee3
Sam Lord <azertify@Sams-Air.home>
2016-01-03 20:15:11 +0000

    removed redundant code

--- src/main/java/chat/haver/server/Main.java
+++ src/main/java/chat/haver/server/Main.java
@@ -1,9 +1,5 @@
 package chat.haver.server;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
